51d881cc5ff3a0c572b5585a60353480ed26c1bb
Dartanum <dartanum2@yandex.ru>
2021-04-03 14:39:32 +0300

    Update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -1,26 +1,25 @@
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.4.22 <0.7.0;
 
 contract Bank {
 
    struct Credit {
-      uint16 percentageRate;
+      uint percentageRate;
       uint currentSum ;
       uint months;
    }
    struct Client {
-      address id;
-      bytes login;
-      bytes password;
-      int balance;
+      address payable id;
+      string login;
+      string password;
+      uint balance;
       Credit credit;
    }
    uint private Storage;
    uint private numberClient;
-   address private owner;
+   address payable private owner;
    Client[] private clients;
 
    constructor () public {
-      //clients = new Client[](10);
       Storage = 100;
       owner = msg.sender;
       numberClient = 0;
@@ -31,48 +30,69 @@ contract Bank {
       _;
    }
 
-   event RegisterError(address id, bytes message);
+   event RegisterError(address id, string message);
 
-   function register(bytes memory login, bytes memory password) public { //clear func
-      bytes memory error = "This login already exist";
+   event RegisterSuccess(address id, string message);
+
+   event TakeCredit(address id, uint percent, uint sum, uint number_month);
+
+   function register(string memory login, string memory password) public payable { //clear func
       bool isExist = false;
       Client memory temp;
-      Client memory newClient = Client(msg.sender, login, password, 0, Credit(0, 0, 0));
+      Client memory newClient = Client(msg.sender, login, password, msg.value, Credit(0, 0, 0));
       for(uint i = 0; i < numberClient; i++) {
          temp = clients[i];
          require(temp.id != msg.sender);
-         if(keccak256(temp.login) == keccak256(newClient.login)) {
-            emit RegisterError(msg.sender, error);
+         if(keccak256(bytes(temp.login)) == keccak256(bytes(newClient.login))) {
+            emit RegisterError(msg.sender, "This login already exist");
             isExist = true;
             break;
          }
       }
       if(!isExist) {
+         emit RegisterSuccess(msg.sender, "Success register");
          clients.push(newClient);
          numberClient++;
       }
    }
 
-   function takeCredit(uint16 percent, uint sum, uint month) public {
+   // function Login(string memory login, string memory password) public returns(Client) {
+   //    Client memory temp;
+   //    Client memory res;
+   //    bool isFind = false;
+   //    for(uint i = 0; i < numberClient; i++) {
+   //       temp = clients[i];
+   //       if(checkClient(temp, login, password)) {
+   //          isFind = true;
+   //          res = temp;
+   //          break;
+   //       }
+   //    }
+   //    return res;
+   // }
+
+   function checkClient(Client memory current, string memory login, string memory password) private pure returns(bool) {
+      return (keccak256(bytes(current.login)) == keccak256(bytes(login))) && (keccak256(bytes(current.password)) == keccak256(bytes(password)));
+   }
+   function takeCredit(string memory login, string memory password, uint percent, uint sum, uint month) public {
       Client memory temp;
       for(uint i = 0; i < numberClient; i++) {
          temp = clients[i];
-         if(temp.id == msg.sender) {
-            clients[i] = Client(temp.id, temp.login, temp.password, temp.balance, Credit(percent, sum, month));
+         if(checkClient(temp, login, password)) {
+            clients[i] = Client(temp.id, temp.login, temp.password, temp.balance + sum, Credit(percent, sum, month));
+            clients[i].id.transfer(sum);
+            Storage -= sum;
+            emit TakeCredit(clients[i].id, percent, sum, month);
             break;
          }
       }
    }
 
-   function checkClientsEqual() private view returns(bool) { //view function (doesn't edit storage variable)
-
-   }
-
-   function pureFunction() private pure returns(int) { //pure function (doesn't read storage variable)
-
-   }
-
    function getStorage() public view checkOwner returns(uint) {
       return Storage;
+   }
+
+   function addToStorage() public payable checkOwner {
+      Storage += msg.value;
    }
 }
